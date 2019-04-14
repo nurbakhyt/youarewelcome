b15c1d6658ca2b37d2e3a62d29a17a02a6283a8a
Nurbakhyt <nurbakhyt@gmail.com>
2019-04-14 23:51:06 +0600

    updated: message's style

--- web/src/components/Message.vue
+++ web/src/components/Message.vue
@@ -1,7 +1,17 @@
 <template>
   <b-row>
-    <b-col :class="{'text-right': message.own}">
-      {{ message.text }}
+    <b-col>
+      <div
+        class="message"
+        :class="{'message--own': message.own}"
+      >
+        <div
+          class="message__text"
+          :class="{'message__text--own': message.own}"
+        >
+          {{ message.text }}
+        </div>
+      </div>
     </b-col>
   </b-row>
 </template>
@@ -19,5 +29,23 @@
 </script>
 
 <style lang="scss">
+  .message {
+    display: flex;
+    margin-bottom: .5em;
 
+    &--own {
+      flex-direction: row-reverse;
+    }
+
+    &__text {
+      display: inline-flex;
+      background-color: #eee;
+      border-radius: 0 1em 1em 1em;
+      padding: .5em 2em;
+
+      &--own {
+        border-radius: 1em 0 1em 1em;
+      }
+    }
+  }
 </style>
